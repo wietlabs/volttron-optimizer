2b8660d535751bbd82c4100e67cc57bb36a66706
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-06-03 12:53:04 +0200

    fix comparision example

--- example_comparision.py
+++ example_comparision.py
@@ -8,8 +8,7 @@ if __name__ == '__main__':
     request1 = Request(1, 'teapot', 0.1 * np.ones(8), 99)
     request2 = Request(2, 'oven', 0.8 * np.ones(6), 99)
 
-    for scheduler_class in NoDelayScheduler, LinearProgrammingScheduler, BruteForceScheduler:
-        scheduler = scheduler_class(lookahead=20)
+    for scheduler in NoDelayScheduler(), LinearProgrammingScheduler(lookahead=20), BruteForceScheduler(lookahead=20):
         hub = Hub(scheduler)
 
         hub.update_source_profile('solar_panel', solar_panel_profile, autoschedule=False)
@@ -17,6 +16,6 @@ if __name__ == '__main__':
         hub.add_request(request2, autoschedule=False)
         hub.schedule()
 
-        scheduler_name = scheduler_class.__name__
+        scheduler_name = scheduler.__class__.__name__
         print(scheduler_name, hub.score)
         hub.visualize(20).savefig(f'img/example_comparision_{scheduler_name}.png')
